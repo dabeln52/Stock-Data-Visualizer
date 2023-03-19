f3b4dae3f06ea5af87ad2a16f8052456a739124d
Afrodinho <54389183+Afrodinho@users.noreply.github.com>
2023-03-19 18:35:58 -0500

    Create Stock_Symbol.py

---
+++ Stock_Symbol.py
@@ -0,0 +1,7 @@
+#Ask user for chart
+print("Please select the type of chart you would like: \n")
+
+#Confirm Selection
+print("Now Showing" + chart")
+#Define Chart
+chart = input("Type Chart Name: \n")
