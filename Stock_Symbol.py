4dd8514ef74bbc44a30cf6a545e0a53f71623a04
Derek Abeln <111763839+dabeln52@users.noreply.github.com>
2023-03-19 16:43:49 -0500

    This code is creating an infinte loop will need to fix that.

--- Stock_Symbol.py
+++ Stock_Symbol.py
@@ -6,23 +6,56 @@ from alpha_vantage.timeseries import TimeSeries
 print("Welcome to your stock Visualizer!\n")
 
 Stock_Symbol = input("Enter Symbol you're looking for here: ")
-
-print('Chart Types')
-print("---------------")
-print('1. Bar')
-print('2. Line')
-chart_type = input('Enter the chart type you want (1, 2): ')
-try:
-    chart_type = int(chart_type)
+chart_options = [1, 2]
+chart_type = None
+while chart_type not in chart_options:
+    print('Chart Types')
+    print("---------------")
+    print('1. Bar')
+    print('2. Line')
+try:    
+    chart_type = int(input('Enter the chart type you want (1, 2): '))
 except ValueError:
     print("Plese pick from option above")
+else:   
     if chart_type == 1:
      chart_type = pygal.Bar()
+     exit
     elif chart_type == 2:
         chart_type = pygal.Line()
-    else:
-       print('Please pick from options above')
+        exit
 
 
+Time_Series_options = [1,2,3,4]
+Time_Series = None
+
+while Time_Series not in Time_Series_options:
+    print("Select the Time Series of the char you want to Generate")
+    print("--------------------------------------------------------")
+    print('1. Intraday')
+    print('2. Daily')
+    print('3. Weekly')
+    print('4. Monthly')
+
+
+try: 
+    Time_Series = int(input('Enter time series option (1, 2, 3, 4):'))
+except ValueError: 
+   print("Please choose from options above.")
+else:
+   if Time_Series == 1:
+      function = 'TIME_SERIES_INTRADAY'
+      exit
+   elif Time_Series == 2:
+      function = 'TIME_SERIES_DAILY'
+      exit
+   elif Time_Series == 3: 
+      function = 'TIME_SERIES_WEEKLY'
+      exit
+   elif Time_Series == 4:
+      function ='TIME_SERIES_MONTHLY'
+      exit
+  
+        
 
 
