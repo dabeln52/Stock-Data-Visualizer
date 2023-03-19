63c91ffec67bdc3cdda84df335ffb7fc290a5068
Derek Abeln <111763839+dabeln52@users.noreply.github.com>
2023-03-19 15:58:45 -0500

    Created first portion of letting user pick the chart type. I used a try catch method to handle exceptions

--- Stock_Symbol.py
+++ Stock_Symbol.py
@@ -2,3 +2,20 @@ print("Welcome to your stock Visualizer!\n")
 
 Stock_Symbol = input("Enter Symbol you're looking for here: ")
 
+print('Chart Types')
+print("---------------")
+print('1. Bar')
+print('2. Line')
+chart_type = input('Enter the chart type you want (1, 2): ')
+try:
+    chart_type = int(chart_type)
+except ValueError:
+    print("Plese pick from option above")
+    if chart_type == 1:
+     chart_type = 'Bar'
+    elif chart_type == '2':
+        chart_type = 'Line'
+    else:
+       print('Please pick from options above')
+
+
