7fb0f33ffc79d9ef69c5d139e9a3c2a66ce58c19
Afrodinho <54389183+Afrodinho@users.noreply.github.com>
2023-03-19 19:03:13 -0500

    Update Stock_Symbol.py

--- Stock_Symbol.py
+++ Stock_Symbol.py
@@ -1,7 +1,30 @@
 #Ask user for chart
-print("Please select the type of chart you would like: \n")
+def chartSelect():
+    print("\nPlease enter the number that corresponds to the chart you would like to view: \n")
 
-#Confirm Selection
-print("Now Showing" + chart")
-#Define Chart
-chart = input("Type Chart Name: \n")
+    print("Chart Types \n--------------\n 1. Bar\n 2. Line\n 3. Exit Program")
+
+    while True:
+        #Define Chart
+        try:
+            chart = int(input("\nI choose chart: "))
+        except:
+            print("This field only accepts numbers. \n Please choose again.\n ")
+        #Confirm Selection
+        if chart == 1: 
+            print("You have chosen Bar Graph!")
+            #Display Corresponding Graph
+            break
+        elif chart == 2:
+            print("You have chosen Line Graph!")
+            #Display Corresponding Graph
+            break
+        elif chart == 3:
+            print("Now Exiting the Program! Goodbye!\n")
+            break
+        
+chartSelect()
+
+
+
+
