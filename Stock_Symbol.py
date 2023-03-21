9233668abdafbf2773e7be1e8adca92b10248db7
Ireland Lyerla <lyerlajd@gmail.com>
2023-03-20 19:47:03 -0500

    pygal line graph
    added some code to read the json response we get and make the line graph

--- Stock_Symbol.py
+++ Stock_Symbol.py
@@ -22,15 +22,15 @@ def startMenu():
             print("\nPlese pick from options above\n")
         else:   
             if chart_type == 1:
-                chart_type = pygal.Bar()
-                break
+                chart_type = "bar"
+                return [Stock_Symbol, chart_type]
             elif chart_type == 2:
-                chart_type = pygal.Line()
-                break
+                chart_type = "line"
+                return [Stock_Symbol, chart_type]
             elif chart_type == 3:
                 print("\nThank You for Using Our Program! Goodbye!\n")
                 quit()
-startMenu()
+
 
 #Selecting the time series
 def timeSeries():
@@ -52,22 +52,17 @@ def timeSeries():
                 continue
         else:
             if Time_Series == 1:
-                function = 'TIME_SERIES_INTRADAY'
-                break
+                return 'TIME_SERIES_INTRADAY'
             elif Time_Series == 2:
-                function = 'TIME_SERIES_DAILY'
-                break
+                return 'TIME_SERIES_DAILY'
             elif Time_Series == 3: 
-                function = 'TIME_SERIES_WEEKLY'
-                break
+                return 'TIME_SERIES_WEEKLY'
             elif Time_Series == 4:
-                function ='TIME_SERIES_MONTHLY'
-                break
+                return 'TIME_SERIES_MONTHLY'
             elif Time_Series == 5:
                 print("\nThank You for Using Our Program! Goodbye!\n")
                 quit()
-     
-timeSeries()
+
   
 #Selecting a date
 #Can be used to get a start date and an end date   
@@ -87,4 +82,63 @@ def date():
             Selected_Date = userResponse
             return Selected_Date
 
-date()
+def compareDates(date1, date2):
+    # part of what we need implement is having a start and stop date, we also need to be able to tell when a date is before another one or not
+    # wouldn't hurt to make a function that compares if date1 is ahead of or after date2
+
+    # also need to be able to 
+    return
+
+def main():
+
+    Stock_Symbol_Chart_Type = startMenu()
+ 
+    function = timeSeries() # will help configure the url, currently does not change anything regardless of answer
+
+    # configure url 
+    
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=16M4EW4M4DAV8AZZ' #this is an example url
+    # url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo' # this is an example url
+    r = requests.get(url)
+    data = r.json()
+    # print(data)
+
+    # making data accessable
+    data = data['Time Series (5min)']
+    # data = data['Time Series (Daily)']
+    for dataPoint in data:
+        print(f"open is {data[dataPoint]['1. open']}\nhigh is {data[dataPoint]['2. high']}\nlow is {data[dataPoint]['3. low']}\nclose is {data[dataPoint]['4. close']}\nvolume is {data[dataPoint]['5. volume']}\n----------------")
+    # dataPoint is just the date, it is used as the key in the dictionary
+
+    # print dates
+    for dataPoint in data:
+        print(dataPoint)
+
+    # bar graph
+    if (Stock_Symbol_Chart_Type[1] == "bar"):
+        # still need to figure out how to use the data to make a useful bar graph
+        return
+
+        
+    # line graph
+    else:
+        chart = pygal.Line()
+        chart.title = "Data from 2023-03-17"
+        open = []
+        high = []
+        low = []
+        close = []
+        for dataPoint in data:
+            open.append(float(data[dataPoint]['1. open']))
+            high.append(float(data[dataPoint]['2. high']))
+            low.append(float(data[dataPoint]['3. low']))
+            close.append(float(data[dataPoint]['4. close']))
+
+        chart.add("Open", open)
+        chart.add("High", high)
+        chart.add("Low", low)
+        chart.add("Close", close)
+        
+        chart.render_in_browser()
+
+main()
