ae2595a6a54ddc9d729ce257cf0b6d4bba8ccf07
James Lyerla <lyerlajd@gmail.com>
2023-03-22 18:39:17 -0500

    intraday, weekly, and monthly
    daily doesn't work because it is a premium feature. also the dates are reverse order on the chart

--- Stock_Symbol.py
+++ Stock_Symbol.py
@@ -30,13 +30,12 @@ def getChart():
             elif chart_type == 2:
                 chart_type = pygal.Line()
                 return chart_type
-
-
-
+            elif chart_type == 3:
+                exit()
 
 #Selecting the time series
 def getTimeSeries():
-    Time_Series_options = [1,2,3,4]
+    Time_Series_options = [1,2,3,4,6]
     Time_Series = None
 
     while Time_Series not in Time_Series_options:
@@ -48,7 +47,7 @@ def getTimeSeries():
         print('4. Monthly')
         print('5. Exit')
         try: 
-            Time_Series = int(input('Enter time series option (1, 2, 3, 4) : '))
+            Time_Series = int(input('Enter time series option (1, 2, 3, 4, 5) : '))
         except ValueError: 
                 print("\nPlease choose from options above.\n")
                 continue
@@ -65,54 +64,131 @@ def getTimeSeries():
             elif Time_Series == 4:
                 Time_Series = 'TIME_SERIES_MONTHLY'
                 return Time_Series
+            elif Time_Series == 5:
+                exit()
 
 
 #Selecting a date
 #Can be used to get a start date and an end date   
-def getStartDate():
-    date = input("Enter a start date in YYYY-MM-DD format: ")
-    return date
+def selectDates():
 
-def getEndDate():
-    endDate = input("Enter a end date in YYYY-MM-DD format: ")
-    #datetime.strptime(endDate, '%Y-%M-%D') 
-    return endDate
+    start_date = None
+    end_date = None
+
+    Date_Error_Message = "That date is not formatted correctly. Please try again."
+
+    # selecting start date
+    while start_date == None:
+        try:
+            userResponse = input("Enter a start date in YYYY-MM-DD format: ")
+            testDateTime = datetime.fromisoformat(userResponse) #used to check formatting and later to compare dates
+        except Exception:
+            print(Date_Error_Message)
+            continue
+
+        start_date = userResponse
+
+    # selecting end date
+    while end_date == None:
+        try:
+            userResponse = input("Enter a end date in YYYY-MM-DD format: ")
+            testDateTime = datetime.fromisoformat(userResponse) #used to check formatting and later to compare dates
+        except Exception:
+            print(Date_Error_Message)
+            continue
+
+        end_date = userResponse
+    
+    return [start_date, end_date]
 
 def compareDates(date1, date2):
-   if date1 < date2:
-    return True
-   else:
-       return False
+   
+    try:
+       date1 = datetime.fromisoformat(date1)
+       date2 = datetime.fromisoformat(date2)
+    except:
+        print("this is an error")
+
+    if date1 <= date2:
+        return True
+    else:
+        return False
    
 
 
 
 def main():
     print("Welcome! ")
-Symbol = getSymbol()
-chart = getChart()
-function = getTimeSeries() # will help configure the url, currently does not change anything regardless of answer
-start_date = getStartDate()
-end_date = getEndDate()
-    # configure url 
-    
-#url = f'https://www.alphavantage.co/query?function={function}&symbol={Symbol}&interval=5min&apikey=16M4EW4M4DAV8AZZ' #this is an example url
-url = f'https://www.alphavantage.co/query?function={function}&symbol={Symbol}&apikey=WNHA95VNGXUV2S6K' # this is an example url
-r = requests.get(url)
-data = r.json()
-    
-date_data ={} 
-for date in data.keys():
-    if date >= start_date and date <= end_date:
-        date_data[date] = data[date]
-        
-chart.title = f'{Symbol} Stock prices for ({start_date} - {end_date})'
-chart.x_labels = sorted(date_data.keys())
-chart.add('Open', [float(date_data[date]['Open']) for date in chart.x_labels])
-chart.add('High', [float(date_data[date]['High']) for date in chart.x_labels])  
-chart.add('Low', [float(date_data[date]['Low']) for date in chart.x_labels])  
-chart.add('Close', [float(date_data[date]['Close']) for date in chart.x_labels])     
+    Symbol = getSymbol()
+    chart = getChart()
+    function = getTimeSeries()
+    date_data = []
+    start_date = None
+    end_date = None
 
-chart.render_in_browser()
+    # making sure start date is before end date and vice versa
+    startAndEndCorrect = False
+    while (startAndEndCorrect == False):
+        dates = selectDates()
+
+        start_date = dates[0]
+        end_date = dates[1]
+
+        startAndEndCorrect = compareDates(start_date, end_date)
+        if (startAndEndCorrect == False): print("Start date must be earlier than end date. Please try again.")
+
+    if (function == "TIME_SERIES_INTRADAY"):
+        # interval can be changed here, I chose 15 minute
+        url = f'https://www.alphavantage.co/query?function={function}&symbol={Symbol}&interval=15min&apikey=16M4EW4M4DAV8AZZ'
+        r = requests.get(url)
+        data = r.json()
+
+        date_data = data['Time Series (15min)'] # any interval changes need to be here too
+
+    elif (function == "TIME_SERIES_DAILY"):
+        url = f'https://www.alphavantage.co/query?function={function}&symbol={Symbol}&apikey=16M4EW4M4DAV8AZZ' # this is an example url
+        r = requests.get(url)
+        data = r.json()
+
+        date_data = data["Time Series (Daily)"]
+
+    elif (function == "TIME_SERIES_WEEKLY"):
+        url = f'https://www.alphavantage.co/query?function={function}&symbol={Symbol}&apikey=16M4EW4M4DAV8AZZ' # this is an example url
+        r = requests.get(url)
+        data = r.json()
+
+        date_data = data['Weekly Time Series']
+
+    else: # function == TIME_SERIES_MONTHLY
+        url = f'https://www.alphavantage.co/query?function={function}&symbol={Symbol}&apikey=16M4EW4M4DAV8AZZ' # this is an example url
+        r = requests.get(url)
+        data = r.json()
+
+        date_data = data['Monthly Time Series']        
+
+    # configuring chart
+    chart.title = f'{Symbol} Stock Prices for ({start_date} - {end_date})'
+    open = []
+    high = []
+    low = []
+    close = []
+    labels = []
+    for date in date_data:
+        if (compareDates(start_date, date) and compareDates(date, end_date)):
+            print(date)
+            print(date_data[date])
+            open.append(float(date_data[date]['1. open']))
+            high.append(float(date_data[date]['2. high']))
+            low.append(float(date_data[date]['3. low']))
+            close.append(float(date_data[date]['4. close']))
+            labels.append(date)
+
+    chart.add('Open', open)
+    chart.add('High', high)
+    chart.add('Low', low)
+    chart.add('Close', close)
+    chart.x_labels = labels
+
+    chart.render_in_browser()
 
 main()
